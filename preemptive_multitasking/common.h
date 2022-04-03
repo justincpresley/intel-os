6e1c7c900aefedb1e78b8eb6d3cd10f1bbd7d1a2
Justin C Presley <justincpresley@gmail.com>
2022-04-03 01:59:42 -0500

    correct CONSOLE values

--- preemptive_multitasking/common.h
+++ preemptive_multitasking/common.h
@@ -4,7 +4,7 @@
 #define NULL ((void *)0)
 #define STACK_SIZE 1024
 #define MAX_PROCESS_ALLOCATIONS 20
-#define CONSOLE_COLS 25
-#define CONSOLE_ROWS 80
+#define CONSOLE_COLS 80
+#define CONSOLE_ROWS 25
 
 #endif//COMMON_H
