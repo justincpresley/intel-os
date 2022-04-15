0bed67d05e0d6f289f0d9a364c2b9085dd3b884e
Justin Presley <justincpresley@gmail.com>
2022-04-15 12:57:23 -0500

    Ensure all requirements are in the install script

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
+apt-get install --no-install-recommends nasm gcc binutils gcc-multilib bximage dosfstools libvcflib-tools mtools make gdb coreutils qemu-system-x86
